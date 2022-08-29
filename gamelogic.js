f4a0480069849b495df5d4b55db74b8c57e6aa28
khans8890 <kansumaira@gmail.com>
2022-08-29 11:25:35 -0400

    feat: pushing up work of apple uploaded

--- gamelogic.js
+++ gamelogic.js
@@ -12,3 +12,29 @@ add([
   move(UP, 5),
 
 ])
+
+// adding the apples
+loadSprite("apple", "apple.png");
+
+// functionality of apple
+add([
+    sprite("apple"),
+    scale(0.05),
+    pos(300, 10),
+    move(DOWN, 40),
+    
+])
+
+// score counter 
+let score = 0;
+const scoreLabel = add([
+    text(score),
+    pos(24, 24)
+])
+
+
+// increment counter when collision happens
+// onUpdate(() => {
+//     score++;
+//     scoreLabel.text = score;
+// });
