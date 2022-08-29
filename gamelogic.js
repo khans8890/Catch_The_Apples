b8bf2be57b97bd705d6e4be8390e839b0764a085
AGarciaNY <aldairgarcia360@gmail.com>
2022-08-29 12:32:35 -0400

    added coalition

--- gamelogic.js
+++ gamelogic.js
@@ -2,13 +2,34 @@
 kaboom();
 
 // add a piece of text at position (120, 80)
-loadSprite("bean", "testingBarrel.png")
+loadSprite("background", "testingbackground.gif")
+loadSprite("barrel", "testingBarrel.png")
 
-// add something to screen
-add([
-  sprite("bean"),
-  scale(0.2),
-  pos(500, 450),
-  move(UP, 5),
+background= add([
+  sprite('background'),
+  pos(width() / 2, height() / 2),
+  origin("center"),
+  // Allow the background to be scaled
+  scale(.6),
+  area(),
+  'tag1',
+  // Keep the background position fixed even when the camera moves
+  fixed()
 
 ])
+
+// add something to screen
+barrel = add([
+  sprite("barrel"),
+  scale(0.2),
+  'tag2',
+  pos(0, 500),
+  area()
+])
+onCollide('tag1', 'tag2', () => { console.log('help')})
+barrel.onUpdate(() => {
+  
+  if (mousePos().x > 86 && mousePos().x < width() - 86 || barrel.pos.x > 0 && barrel.pos.x < width() - 172) {
+    barrel.moveTo(mousePos().x - 86, 500, 400)
+  }
+})
