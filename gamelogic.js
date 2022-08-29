dfad5fbfb105b90b79ede96c6ff6142288f370bb
AGarciaNY <aldairgarcia360@gmail.com>
2022-08-29 14:24:33 -0400

    added power up

--- gamelogic.js
+++ gamelogic.js
@@ -4,20 +4,41 @@ kaboom();
 // add a piece of text at position (120, 80)
 loadSprite("background", "testingbackground.gif")
 loadSprite("barrel", "testingBarrel.png")
+loadSprite('speedPower', 'testingPower.jpg')
 
 background= add([
   sprite('background'),
-  pos(width() / 2, height() / 2),
+  pos(width() / 3, height() / 2),
   origin("center"),
   // Allow the background to be scaled
   scale(.6),
   area(),
-  'tag1',
   // Keep the background position fixed even when the camera moves
   fixed()
 
 ])
+// power = add([
+//   sprite('speedPower'),
+//   pos(rand(vec2(width())).x, 10),
+//   'tag1',
+//   area(),
+//   scale(.2),
+//   move(DOWN, 240),
 
+// ])
+let count = 1
+loop(rand(20,25), () => {
+  // add tree
+  add([
+    sprite('speedPower'),
+    pos(rand(vec2(width())).x, 10),
+    `tag1`,
+    area(),
+    scale(.2),
+    move(DOWN, 240),
+  ])
+  count++
+});
 // add something to screen
 barrel = add([
   sprite("barrel"),
@@ -26,10 +47,20 @@ barrel = add([
   pos(0, 500),
   area()
 ])
-onCollide('tag1', 'tag2', () => { console.log('help')})
+console.log(rand(vec2(width())).x)
+let barrelSpeed = 400
+
+
 barrel.onUpdate(() => {
-  
-  if (mousePos().x > 86 && mousePos().x < width() - 86 || barrel.pos.x > 0 && barrel.pos.x < width() - 172) {
-    barrel.moveTo(mousePos().x - 86, 500, 400)
+  if (mousePos().x > 86 && mousePos().x < width() - 86 || barrel.pos.x > 0 && barrel.pos.x < width() - 170) {
+    barrel.moveTo(mousePos().x - 86, 500, barrelSpeed)
   }
+})
+barrel.onCollide('tag1', (power) => {
+  barrelSpeed = 1000
+  destroy(power)
+  wait(10, () => {
+    console.log('2')
+    barrelSpeed = 400
+  })
 })
