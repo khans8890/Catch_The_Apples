c21b8de8b5a6bea78eaa967aa193bef7ccc44891
AGarciaNY <aldairgarcia360@gmail.com>
2022-08-30 14:44:08 -0400

    feat: add scenes to our game

--- gamelogic.js
+++ gamelogic.js
@@ -7,140 +7,165 @@ loadSprite('speedPower', 'testingPower.jpg')
 loadSprite("apple", "apple.png");
 gravity(80)
 
-let score = 0;
-let countDown = 60;
-let timeOfGame = 120;
+var score = 0;
+var countDown = 60;
+let timeOfGame = 5;
 
 // background
 loadSprite("clearsky", "background2.jpg")
 
-let background = add([
-  sprite("clearsky"),
-  pos(width() / 2, height() / 2),
-  origin("center"),
-  scale(2),
-  fixed()
-]);
 
-add([
-  pos(0, height() - 100),
-  rect(width(), 100),
-  outline(4),
-  'floor',
-  area(),
-  color(102, 189, 88)
-])
-onCollide('fallingApple', 'floor', (ap,) => {
-  destroy(ap)
-})
+scene("game", () => {
+  let background = add([
+    sprite("clearsky"),
+    pos(width() / 2, height() / 2),
+    origin("center"),
+    scale(2),
+    fixed()
+  ]);
 
-let scoreText = add([
-  text(`Score: ${score}`),
-  pos(10, height() - 100)
-])
+  add([
+    pos(0, height() - 100),
+    rect(width(), 100),
+    outline(4),
+    'floor',
+    area(),
+    outline(0.005),
 
-let timer = add([
-  text(`Timer:${new Date(timeOfGame * 1000).toISOString().substring(14, 19)}`),
-  pos(width() - 520,height()-100)
-])
+    color(102, 189, 88)
+  ])
+  onCollide('fallingApple', 'floor', (ap,) => {
+    destroy(ap)
+  })
+
+  let scoreText = add([
+    text(`Score: ${score}`),
+    pos(10, height() - 100)
+  ])
+
+  let timer = add([
+    text(`Timer:${new Date(timeOfGame * 1000).toISOString().substring(14, 19)}`),
+    pos(width() - 520, height() - 100)
+  ])
 
 
-// dom elements
-const audio = document.querySelector("audio");
-const playAudio = document.body.querySelectorAll("button")[0];
-const pauseAudio = document.body.querySelectorAll("button")[1];
+  // dom elements
+  const audio = document.querySelector("audio");
+  const playAudio = document.body.querySelectorAll("button")[0];
+  const pauseAudio = document.body.querySelectorAll("button")[1];
 
-// event listeners 
+  // event listeners 
   playAudio.addEventListener("click", () => {
-  if(!audio.play()){
-  audio.play(); 
-  }
+    if (!audio.play()) {
+      audio.play();
+    }
   });
   pauseAudio.addEventListener("click", () => {
-  if(!audio.pause()){
-  audio.pause();
-  }
+    if (!audio.pause()) {
+      audio.pause();
+    }
   });
 
 
 
-loop(rand(25,30), () => {
-  // add tree
-  add([
-    sprite('speedPower'),
-    pos(rand(vec2(width())).x, 10),
-    `tag1`,
-    area(),
-    scale(.2),
-    move(DOWN, 240),
-  ])
-});
-
-barrel = add([
-  sprite("barrel"),
-  scale(0.2),
-  'tag2',
-  pos(0, 500),
-  area()
-])
-
-let barrelSpeed = 400
-
-barrel.onUpdate(() => {
-  if (mousePos().x > 86 && mousePos().x < width() - 86 || barrel.pos.x > 0 && barrel.pos.x < width() - 170) {
-    barrel.moveTo(mousePos().x - 86, 500, barrelSpeed)
-  }
-})
-
-barrel.onCollide('tag1', (power) => {
-  barrelSpeed = 1000
-  destroy(power)
-  wait(10, () => {
-    barrelSpeed = 400
-  })
-})
-let spawnSpeed = 1
-let appleFallSpeed = 150
-let spawnRate = 1;
-
-// functionality of apple
-loop(spawnSpeed, () => {
-  for (let i = 0; i < spawnRate; i++) {
+  loop(rand(25, 30), () => {
+    // add tree
     add([
-      sprite("apple"),
-      scale(0.05),
-      pos(rand(vec2(width())).x, -100),
-      //move(DOWN, rand(appleFallSpeed-100, appleFallSpeed+100)),
+      sprite('speedPower'),
+      pos(rand(vec2(width())).x, 10),
+      `tag1`,
       area(),
-      body(),
-      'fallingApple',
+      scale(.2),
+      move(DOWN, 240),
     ])
-  }
-})
+  });
 
-barrel.onCollide('fallingApple', (fallingApple) => {
-  destroy(fallingApple);
-  score+=5;
-  scoreText.text = `Score: ${score}`;
-})
+  barrel = add([
+    sprite("barrel"),
+    scale(0.2),
+    'tag2',
+    pos(0, 500),
+    area()
+  ])
 
-//timer
-loop(1, () => { 
-  countDown--
-  if (countDown % 10 === 0) { 
-    if (spawnSpeed > 0.3) {
-      spawnSpeed -= 0.2
-    } else if (spawnSpeed > 0.05) { 
-      spawnSpeed = spawnSpeed - 0.02
+  let barrelSpeed = 400
+
+  barrel.onUpdate(() => {
+    if (mousePos().x > 86 && mousePos().x < width() - 86 || barrel.pos.x > 0 && barrel.pos.x < width() - 170) {
+      barrel.moveTo(mousePos().x - 86, 500, barrelSpeed)
     }
-    if (appleFallSpeed < 500) {
-      appleFallSpeed += 50
+  })
+
+  barrel.onCollide('tag1', (power) => {
+    barrelSpeed = 1000
+    destroy(power)
+    wait(10, () => {
+      barrelSpeed = 400
+    })
+  })
+  let spawnSpeed = 1
+  let appleFallSpeed = 150
+  let spawnRate = 1;
+
+  // functionality of apple
+  loop(spawnSpeed, () => {
+    for (let i = 0; i < spawnRate; i++) {
+      add([
+        sprite("apple"),
+        scale(0.05),
+        pos(rand(vec2(width())).x, -100),
+        //move(DOWN, rand(appleFallSpeed-100, appleFallSpeed+100)),
+        area(),
+        body(),
+        'fallingApple',
+      ])
     }
-  }
-  if (countDown % 20 === 0 && spawnRate < 10) { 
-    spawnRate++
-  }
-  timeOfGame--
-  timer.text = `Timer:${new Date(timeOfGame * 1000).toISOString().substring(14, 19)}`;
+  })
+
+  barrel.onCollide('fallingApple', (fallingApple) => {
+    destroy(fallingApple);
+    score += 5;
+    scoreText.text = `Score: ${score}`;
+  })
+
+  //timer
+  loop(1, () => {
+    countDown--
+    
+    if (countDown % 10 === 0) {
+      if (spawnSpeed > 0.3) {
+        spawnSpeed -= 0.2
+      } else if (spawnSpeed > 0.05) {
+        spawnSpeed = spawnSpeed - 0.02
+      }
+      if (appleFallSpeed < 500) {
+        appleFallSpeed += 50
+      }
+    }
+    if (countDown % 20 === 0 && spawnRate < 10) {
+      spawnRate++
+    }
+
+    timeOfGame--
+    timer.text = `Timer:${new Date(timeOfGame * 1000).toISOString().substring(14, 19)}`;
+    if (timeOfGame === 0) {
+      go('gameOver')
+    }
+  })
+
 })
+scene("gameOver", () => {
+  let scoreText = add([
+    text(`GameOver`),
+    pos(10, height() - 100)
+  ])
+ })
+function isGameOver() { 
+  if (timeOfGame > 0) {
+    go('game')
+  } else {
+    go('gameOver')
+  }
+}
+isGameOver()
 
