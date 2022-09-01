7d584e162ae95c7741d4630fa6ea43ada41bfaf6
AGarciaNY <aldairgarcia360@gmail.com>
2022-08-31 22:18:48 -0400

    feat: added score board for two players and made the lobby

--- gamelogic.js
+++ gamelogic.js
@@ -3,6 +3,8 @@ import { getDatabase, onValue, ref, set } from "https://www.gstatic.com/firebase
 import db from './database.js';
 let isTwoPayer = false;
 let playerIs = 'playerOne'
+let gameKeyTojoin;
+gameKeyTojoin = 'jmfSLwU5/'
 // function makeid(length) {
 //   var result = '';
 //   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -18,30 +20,39 @@ let gamedata = {
 }
 let setPlayerTwoButton = document.getElementById('playerteobutton')
 setPlayerTwoButton.addEventListener('click', () => {
-  playerIs = 'playerTwo'
+  playerIs = 'playerTwo';
+  console.log(playerIs);
 })
 let getPlayerOnesavedScore;
 let getPlayerTwosavedScore;
 var playerOnesavedScore = 0;
 var playerTwosavedScore = 0;
-
+var getPlayerTwoReady;
+var getDidGameStart;
+let timeOfGame;
 let button = document.getElementById('removesthis')
 button.addEventListener('click', () => {
   isTwoPayer = true
-  gamedata.reference = ref(db, 'jmfSLwU5/')
-  getPlayerOnesavedScore = ref(db, 'jmfSLwU5/', 'playerOnePoints');
-  getPlayerTwosavedScore = ref(db, 'jmfSLwU5/', 'playerTwoPoints');
+  gamedata.reference = ref(db, gameKeyTojoin)
+  getPlayerOnesavedScore = ref(db, gameKeyTojoin, 'playerOnePoints');
+  getPlayerTwosavedScore = ref(db, gameKeyTojoin, 'playerTwoPoints');
   set(gamedata.reference, {
     playerOnePoints: 0,
-    playerTwoPoints: 0
+    playerTwoPoints: 0,
+    isPlayerTwoReady: false,
+    didGameStart: false,
+    gameTimer: 120
   })
   onValue(getPlayerOnesavedScore, (snap) => {
     playerOnesavedScore = snap.val().playerOnePoints;
+    timeOfGame = snap.val().gameTimer;
   })
   onValue(getPlayerTwosavedScore, (snap) => {
     playerTwosavedScore = snap.val().playerTwoPoints;
+    getPlayerTwoReady = snap.val().isPlayerTwoReady;
+    getDidGameStart = snap.val().didGameStart;
   })
-  console.log(playerTwosavedScore)
+  console.log('twoPlayer game')
 })
   
 
@@ -57,16 +68,115 @@ loadSprite("badApple", "badApple.png");
 gravity(80)
 loadSprite("reset", "restart.png")
 
-var score = 4;
+var score = 0;
 var countDown = 60;
-let timeOfGame = 120;
+
 
 // background
-loadSprite("clearsky", "background2.jpg")
+loadSprite("clearsky", "background2.jpg");
+loadSprite('readyButtonIMG', 'ready.jpg');
 
+///waitingLobby -------------------------------------------------------------------------------------------------------------------------------------------------
+scene('waitingLobby', () => {
+  let background = add([
+    sprite("clearsky"),
+    pos(width() / 2, height() / 2),
+    origin("center"),
+    scale(2),
+    fixed()
+  ]);
+  let readyButton = add([
+    sprite("readyButtonIMG"),
+    pos(width() / 2, (height() / 2) - 100),
+    origin("center"),
+    'ImReady',
+    scale(.5),
+    area(),
+    fixed()
+  ]);
+  add([
+    sprite("readyButtonIMG"),
+    pos(width() / 2+200, (height() / 2) - 100),
+    origin("center"),
+    'startTheGame',
+    scale(.2),
+    area(),
+    fixed()
+  ]);
+  add([
+    text(`are you ready`, {
+      size: 30
+    }),
+    pos(100, 160)
+  ])
+  let playerOnetext = add([
+    text(`Player One is ready`, {
+      size: 30
+    }),
+    pos(100, 200)
+  ])
+  playerOnetext.width = 100;
 
+  add([
+    text(`Player Two is not ready`, {
+      size: 30
+    }),
+    pos(100, 250)
+  ])
+  if (playerIs === "playerOne") {
+    let enterKeyToJoin = document.createElement('input')
+    document.body.append(enterKeyToJoin)
+    enterKeyToJoin.setAttribute("type", "text");
+    enterKeyToJoin.setAttribute("id", "GetGivenKey");
+    enterKeyToJoin.style.position = "absolute"
+    enterKeyToJoin.style.top = `${(height()/2) - 40}px`
+    enterKeyToJoin.style.left = `${(width() / 2) - 40}px`
+
+    let keySubmmitButton = document.createElement('button')
+    document.body.append(keySubmmitButton)
+    keySubmmitButton.innerText = 'Submit'
+    keySubmmitButton.style.position = "absolute"
+    keySubmmitButton.style.top = `${(height() / 2-10)}px`
+    keySubmmitButton.style.left = `${(width() / 2) +20}px`
+    keySubmmitButton.addEventListener('click', () => { 
+      gameKeyTojoin = enterKeyToJoin.value;
+      console.log(gameKeyTojoin)
+    })
+  }
+  onClick('ImReady', () => {
+    if (playerIs === 'playerTwo') {
+      set(getPlayerOnesavedScore, {
+        playerOnePoints: playerOnesavedScore,
+        playerTwoPoints: playerTwosavedScore,
+        isPlayerTwoReady: true,
+        gameTimer: timeOfGame
+      })
+    } 
+    console.log('cats')
+  })
+  onClick('startTheGame', () => {
+    if (getPlayerTwoReady) {
+      set(getPlayerOnesavedScore, {
+        playerOnePoints: playerOnesavedScore,
+        playerTwoPoints: playerTwosavedScore,
+        isPlayerTwoReady: true,
+        didGameStart: true,
+        gameTimer: timeOfGame
+      })
+      console.log('start game')
+    } else {
+      console.log('playerTwo is not ready')
+    }
+  })
+  readyButton.onUpdate(() => { 
+    if (getDidGameStart) { 
+      go('game')
+    }
+  })
+})
+go('waitingLobby')
+///game -------------------------------------------------------------------------------------------------------------------------------------------------
 scene("game", () => {
-
   let background = add([
     sprite("clearsky"),
     pos(width() / 2, height() / 2),
@@ -94,18 +204,34 @@ scene("game", () => {
   onCollide('newBadApple', 'tag2', (ap,) => {
     score -= 10;
     destroy(ap)
-    scoreText.text = `Score: ${score}`;
+    if (!isTwoPayer) {
+      scoreText.text = `Score: ${score}`;
+    }
   })
+  if (!isTwoPayer) {
+    var scoreText = add([
+      text(`Score: ${score}`),
+      pos(10, height() - 100)
+    ])
+    var timer = add([
+      text(`Timer:${new Date(timeOfGame * 1000).toISOString().substring(14, 19)}`, { letterSpacing: 0, letterSpacing: 3}),
+      pos(width() - 520, height() - 100)
+    ])
+  } else {
+    var playerOneScoreText = add([
+      text(`P1 Score: ${score}`, { letterSpacing: 0, letterSpacing: 3, size: 50 }),
+      pos(10, height() - 100)
+    ])
+    var playerTwoScoreText = add([
+      text(`P2 Score: ${score}`, { letterSpacing: 0, letterSpacing: 3, size: 50 }),
+      pos(width()-600, height() - 100)
+    ])
+    var timer = add([
+      text(`Timer:${new Date(timeOfGame * 1000).toISOString().substring(14, 19)}`, { letterSpacing: 0, size: 50 }),
+      pos(width()/2 - 300, height() - 100)
+    ])
+  }
 
-  let scoreText = add([
-    text(`Score: ${score}`),
-    pos(10, height() - 100)
-  ])
-
-  let timer = add([
-    text(`Timer:${new Date(timeOfGame * 1000).toISOString().substring(14, 19)}`),
-    pos(width() - 520, height() - 100)
-  ])
 
 
   // dom elements
@@ -125,7 +251,7 @@ scene("game", () => {
     }
   });
 
-  loop(rand(25, 30), () => {
+  loop(10, () => {
     // add tree
     add([
       sprite('speedPower'),
@@ -151,12 +277,14 @@ scene("game", () => {
     if (mousePos().x > 86 && mousePos().x < width() - 86 || barrel.pos.x > 0 && barrel.pos.x < width() - 170) {
       barrel.moveTo(mousePos().x - 86, 500, barrelSpeed)
     }
+    playerOneScoreText.text = `P1 Score: ${playerOnesavedScore}`;
+    playerTwoScoreText.text = `P2 Score: ${playerTwosavedScore}`;
   })
 
   barrel.onCollide('tag1', (power) => {
     barrelSpeed = 1000
     destroy(power)
-    wait(10, () => {
+    wait(5, () => {
       barrelSpeed = 400
     })
   })
@@ -192,7 +320,12 @@ scene("game", () => {
   barrel.onCollide('fallingApple', (fallingApple) => {
     destroy(fallingApple);
     score += 5;
-    scoreText.text = `Score: ${score}`;
+    if (!isTwoPayer) {
+      scoreText.text = `Score: ${score}`;
+    } else {
+      playerOneScoreText.text = `P1 Score: ${playerOnesavedScore}`;
+      playerTwoScoreText.text = `P2 Score: ${playerTwosavedScore}`;
+    }
   })
 
   // timer
@@ -212,23 +345,40 @@ scene("game", () => {
     if (countDown % 20 === 0 && spawnRate < 10) {
       spawnRate++
     }
+    if (playerIs === 'playerOne') {
+      timeOfGame--
+      timer.text = `Timer:${new Date(timeOfGame * 1000).toISOString().substring(14, 19)}`;
+      set(getPlayerOnesavedScore, {
+        playerOnePoints: playerOnesavedScore,
+        playerTwoPoints: playerTwosavedScore,
+        gameTimer: timeOfGame
+      })
+    } else {
+      timeOfGame--
+      timer.text = `Timer:${new Date(timeOfGame * 1000).toISOString().substring(14, 19)}`;
+      set(getPlayerOnesavedScore, {
+        playerOnePoints: playerOnesavedScore,
+        playerTwoPoints: playerTwosavedScore,
+        gameTimer: timeOfGame
+      })
+    }
 
-    timeOfGame--
-    timer.text = `Timer:${new Date(timeOfGame * 1000).toISOString().substring(14, 19)}`;
-    if (timeOfGame === 0) {
+    if (timeOfGame <= 0) {
       go('gameOver')
     }
 
-    if (isTwoPayer) { 
+    if (isTwoPayer) {
       if (playerIs === 'playerOne') {
         set(getPlayerOnesavedScore, {
           playerOnePoints: score,
-          playerTwoPoints: playerTwosavedScore
+          playerTwoPoints: playerTwosavedScore,
+          gameTimer: timeOfGame
         })
       } else {
         set(getPlayerTwosavedScore, {
           playerOnePoints: playerOnesavedScore,
           playerTwoPoints: score,
+          gameTimer: timeOfGame
         })
       }
     }
@@ -277,11 +427,3 @@ onClick("reset-button",()=>{
 })
 });
 
-function isGameOver() { 
-  if (timeOfGame > 0) {
-    go('game');
-  } else {
-    go('gameOver');
-  }
-}
-isGameOver();
